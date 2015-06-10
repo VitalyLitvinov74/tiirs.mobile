e20eb89e069ab1c8782f78c9e24e529b2f4f63b7
Dmitriy Logachov <demonwork@yandex.ru>
2015-06-10 19:25:42 +0500

    Добавлен класс таблицы task_status

---
+++ Toir-mobile/src/ru/toir/mobile/db/adapters/TaskStatusDBAdapter.java
@@ -0,0 +1,102 @@
+package ru.toir.mobile.db.adapters;
+
+import ru.toir.mobile.DatabaseHelper;
+import ru.toir.mobile.TOiRDBAdapter;
+import ru.toir.mobile.db.tables.TaskStatus;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class TaskStatusDBAdapter {
+	private DatabaseHelper mDbHelper;
+	private SQLiteDatabase mDb;
+	private final Context mContext;
+
+	public static final String TABLE_NAME = "task_status";
+	
+	public static final String FIELD_UUID_NAME = "uuid";
+	public static final String FIELD_TITLE_NAME = "title";
+	
+	String[] mColumns = {
+			FIELD_UUID_NAME,
+			FIELD_TITLE_NAME};
+		
+	/**
+	 * @param context
+	 * @return OrderDBAdapter
+	 */
+	public TaskStatusDBAdapter(Context context) {
+		mContext = context;
+	}
+	
+	/**
+	 * Открываем базу данных
+	 */
+	public TaskStatusDBAdapter open() {
+		mDbHelper = new DatabaseHelper(mContext, TOiRDBAdapter.getDbName(), null, TOiRDBAdapter.getAppDbVersion());
+		mDb = mDbHelper.getWritableDatabase();
+		return this;
+	}
+	
+	/**
+	 * Закрываем базу данных
+	 */
+	public void close() {
+		mDb.close();
+		mDbHelper.close();
+	}
+
+	/**
+	 * 
+	 * @param uuid
+	 * @return
+	 */
+	public Cursor getItem(String uuid) {		
+		Cursor cursor;
+		cursor = mDb.query(TABLE_NAME, mColumns, FIELD_UUID_NAME + "=?", new String[]{uuid}, null, null, null);		
+		if (cursor.moveToFirst()) {
+			return cursor;
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Cursor getAllItems() {
+		Cursor cursor;
+		cursor = mDb.query(TABLE_NAME, mColumns, null, null, null, null, null);		
+		if (cursor.moveToFirst()) {
+			return cursor;
+		}
+		return null;
+	}
+	
+	/**
+	 * <p>Добавляет/изменяет запись в таблице task_status</p>
+	 * @param uuid
+	 * @param title
+	 * @return
+	 */
+	public long replace(String uuid, String title) {
+		// TODO нужно сделать контроль, выполнилось выражение или нет
+		long id;
+		ContentValues values = new ContentValues();
+		values.put(FIELD_UUID_NAME, uuid);
+		values.put(FIELD_TITLE_NAME, title);
+		id = mDb.replace(TABLE_NAME, null, values);
+		return id;
+	}
+
+	/**
+	 * <p>Добавляет/изменяет запись в таблице task_status</p>
+	 * 
+	 * @param token
+	 * @return long id столбца или -1 если не удалось добавить запись
+	 */
+	public long replace(TaskStatus status) {
+		return replace(status.getUuid(), status.getTitle());
+	}
+}
