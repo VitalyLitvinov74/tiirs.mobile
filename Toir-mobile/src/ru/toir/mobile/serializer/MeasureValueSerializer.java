c0a7c199a8d9a9a4183c38bb9af640836b04d512
Dmitriy Logachov <demonwork@yandex.ru>
2015-10-06 10:59:50 +0500

    Реализована отправка бинарных данных результатов измерений. Удалены лишнии сообщения.

--- Toir-mobile/src/ru/toir/mobile/serializer/MeasureValueSerializer.java
+++ Toir-mobile/src/ru/toir/mobile/serializer/MeasureValueSerializer.java
@@ -3,9 +3,12 @@
  */
 package ru.toir.mobile.serializer;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.lang.reflect.Type;
 import ru.toir.mobile.serverapi.result.MeasureValueRes;
 import ru.toir.mobile.utils.DataUtils;
+import android.util.Base64;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
@@ -27,7 +30,26 @@ public class MeasureValueSerializer implements JsonSerializer<MeasureValueRes> {
 		JsonObject result = new JsonObject();
 		result.addProperty("Id", item.getUuid());
 		result.addProperty("OperationPatternStepResultId", item.getOperation_pattern_step_result_uuid());
-		result.addProperty("Value", item.getValue());
+		String value = item.getValue();
+		result.addProperty("Value", value);
+		// пока тупо считаем что если значение начинается с '/' то это файл
+		if (value.startsWith("/")) {
+			File file = new File(value);
+			int bufferSize = 1024;
+			int count;
+			byte buffer[] = new byte[bufferSize];
+			StringBuilder valueBase64 = new StringBuilder();
+			try {
+				FileInputStream fis = new FileInputStream(file);
+				while ((count = fis.read(buffer)) > 0) {
+					valueBase64.append(Base64.encodeToString(buffer, 0, count, Base64.NO_CLOSE));
+				}
+				result.addProperty("BinaryValue", valueBase64.toString());
+				fis.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		result.addProperty("CreatedAt", DataUtils.getDate(item.getCreatedAt(), "yyyy-MM-dd hh:mm:ss"));
 		result.addProperty("ChangedAt", DataUtils.getDate(item.getChangedAt(), "yyyy-MM-dd hh:mm:ss"));
 		
