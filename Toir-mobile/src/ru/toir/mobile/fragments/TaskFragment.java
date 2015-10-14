d9ddb2715095e00ca6bb59abf5ba0575a515ca4d
Shtrm <Shtrm@192.168.8.185>
2015-10-14 14:44:31 +0500

    изменил иконку приложения

--- Toir-mobile/src/ru/toir/mobile/fragments/TaskFragment.java
+++ Toir-mobile/src/ru/toir/mobile/fragments/TaskFragment.java
@@ -42,7 +42,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
-import android.net.Uri;
+//import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
