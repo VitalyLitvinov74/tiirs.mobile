a1f992496ed2732898f50ccc8abfe85e0d4ffff2
Dmitriy Logachov <demonwork@yandex.ru>
2015-10-15 14:48:10 +0500

    Реализованы в первом приближении методы работы со считывателем и метками драйвера RfidDriverC5. Удалены константы типов операций, выполняемых в основном приложении.

--- Toir-mobile/src/ru/toir/mobile/rfid/driver/RfidDriverC5.java
+++ Toir-mobile/src/ru/toir/mobile/rfid/driver/RfidDriverC5.java
@@ -24,21 +24,12 @@ public class RfidDriverC5 extends RfidDriverBase implements IRfidDriver {
 
 	private static final String TAG = "RfidDriverC5";
 
-	public final static int READ_USER_LABLE = 1;
-	public final static int READ_EQUIPMENT_LABLE_ID = 2;
-	public final static int READ_EQUIPMENT_OPERATION_LABLE_ID = 3;
-	public final static int READ_EQUIPMENT_MEMORY = 4;
-	public final static int READ_EQUIPMENT_OPERATION_MEMORY = 5;
-	public final static int WRITE_EQUIPMENT_OPERATION_MEMORY = 6;
-	public final static int WRITE_EQUIPMENT_MEMORY = 7;
-	public final static int WRITE_USER_MEMORY = 8;
+	public final static int MEMORY_BANK_RESERVED = 0;
+	public final static int MEMORY_BANK_EPC = 1;
+	public final static int MEMORY_BANK_TID = 2;
+	public final static int MEMORY_BANK_USER = 3;
 
-	public final static int USER_MEMORY_BANK = 3;
-
-	private Handler c5Handler = new MainHandler();
 	private static String m_strresult = "";
-
-	private static String mPCEPC = "";
 
 	public RfidDriverC5(DialogFragment dialog, Handler handler) {
 		super(dialog, handler);
@@ -49,7 +40,6 @@ public class RfidDriverC5 extends RfidDriverBase implements IRfidDriver {
 
 		Log.d(TAG, "init");
 
-		// reader.m_handler = c5Handler;
 		android.hardware.uhf.magic.reader.init("/dev/ttyMT1");
 		android.hardware.uhf.magic.reader.Open("/dev/ttyMT1");
 		if (reader.SetTransmissionPower(1950) != 0x11) {
@@ -96,70 +86,40 @@ public class RfidDriverC5 extends RfidDriverBase implements IRfidDriver {
 		}
 	};
 
-	static private class MainHandler extends Handler {
+	static private class ReadTagDataHandler extends Handler {
 		@Override
 		public void handleMessage(Message message) {
-			
-			int types = -1;
 			if (message.what != 0) {
 				if (m_strresult.indexOf((String) message.obj) < 0) {
 					Log.d(TAG, (String) message.obj);
 					m_strresult += (String) message.obj;
-					// возврат при чтении метки пользователя
-					if (types == READ_USER_LABLE || types == 0) {
-						// m_strresult = "01234567";
-						reader.StopLoop();
+					reader.StopLoop();
+					Message result = new Message();
+					result.arg1 = RfidDriverBase.RESULT_RFID_SUCCESS;
+					Bundle bundle = new Bundle();
+					bundle.putString(RfidDriverBase.RESULT_RFID_TAG_ID,
+							m_strresult);
+					result.setData(bundle);
+					mHandler.sendMessage(result);
 
-						Message result = new Message();
-						result.arg1 = RfidDriverBase.RESULT_RFID_SUCCESS;
-						Bundle bundle = new Bundle();
-						bundle.putString(RfidDriverBase.RESULT_RFID_TAG_ID,
-								m_strresult);
-						result.setData(bundle);
-						mHandler.sendMessage(result);
-					}
-					// возврат при чтении метки оборудования
-					if (types == READ_EQUIPMENT_LABLE_ID
-							|| types == READ_EQUIPMENT_OPERATION_LABLE_ID) {
-						reader.m_strPCEPC = m_strresult;
-						mPCEPC = m_strresult;
-						if (types == READ_EQUIPMENT_LABLE_ID) {
-							// ((EquipmentInfoActivity)
-							// mActivity).CallbackOnReadLable(m_strresult);
-						}
-						if (types == READ_EQUIPMENT_OPERATION_LABLE_ID) {
-							// ((OperationActivity)
-							// mActivity).CallbackOnReadLable(m_strresult);
-						}
-					}
-					// возврат при чтении памяти оборудования
-					if (types == READ_EQUIPMENT_MEMORY
-							|| types == READ_EQUIPMENT_OPERATION_MEMORY) {
-						if (mPCEPC != null && !mPCEPC.equals("")) {
-							if (types == READ_EQUIPMENT_MEMORY) {
-								// ((EquipmentInfoActivity)
-								// mActivity).Callback(m_strresult);
-							}
-							if (types == READ_EQUIPMENT_OPERATION_MEMORY) {
-								// ((OperationActivity)
-								// mActivity).Callback(m_strresult);
-							}
-						}
-					}
-					// возврат при записи памяти оборудования
-					if (types == WRITE_EQUIPMENT_OPERATION_MEMORY) {
-						// ((OperationActivity)
-						// mActivity).CallbackOnWrite(m_strresult);
-					}
-					// возврат при записи памяти оборудования
-					if (types == WRITE_EQUIPMENT_MEMORY
-							|| types == WRITE_USER_MEMORY) {
-						// ((EquipmentInfoActivity)
-						// mActivity).CallbackOnWrite(m_strresult);
-					}
-					// m_strresult="";
 				}
-				// android.hardware.uhf.magic.reader.StopLoop();
+			}
+		}
+	};
+
+	static private class writeTagDataHandler extends Handler {
+		@Override
+		public void handleMessage(Message message) {
+
+			// TODO реализовать обработку сообщений считывателя во время записи
+			// в метку
+			if (message.what != 0) {
+				if (m_strresult.indexOf((String) message.obj) < 0) {
+					Log.d(TAG, (String) message.obj);
+					m_strresult += (String) message.obj;
+					// возврат при записи памяти оборудования
+					// ((???) mActivity).CallbackOnWrite(m_strresult);
+				}
 			}
 		}
 	};
@@ -175,80 +135,45 @@ public class RfidDriverC5 extends RfidDriverBase implements IRfidDriver {
 	public void readTagData(String password, int memoryBank, int address,
 			int count) {
 
+		// TODO реализовать получение Id метки
+		// TODO реализовать чтение метки по полученому Id
 	}
 
 	@Override
 	public void readTagData(String password, String tagId, int memoryBank,
 			int address, int count) {
-		/*
-		 * //types = type; types = -1; // scanText = (TextView)
-		 * mActivity.findViewById(R.id.code_from_bar); if (types <=
-		 * READ_USER_LABLE)
-		 * android.hardware.uhf.magic.reader.InventoryLablesLoop(); if (types ==
-		 * READ_EQUIPMENT_LABLE_ID || types ==
-		 * READ_EQUIPMENT_OPERATION_LABLE_ID)
-		 * android.hardware.uhf.magic.reader.InventoryLables(); if (types ==
-		 * READ_EQUIPMENT_MEMORY || types == READ_EQUIPMENT_OPERATION_MEMORY) {
-		 * reader.m_strPCEPC = mPCEPC; byte[] epc =
-		 * reader.stringToBytes(reader.m_strPCEPC); byte memoryBank =
-		 * USER_MEMORY_BANK; // user memory int address = 0; // читаем всегда с
-		 * начала int dataLength = 32; // длина памяти данных String
-		 * passwordString = "00000000"; // пароль byte[] password =
-		 * reader.stringToBytes(passwordString); try { Thread.sleep(690); }
-		 * catch (InterruptedException e) { e.printStackTrace(); } m_strresult =
-		 * ""; reader.ReadLables(password, epc.length, epc, memoryBank, address,
-		 * dataLength); } // reader.m_strPCEPC = "";
-		 */
+
+		m_strresult = "";
+		reader.m_handler = new ReadTagDataHandler();
+		reader.ReadLables(password.getBytes(), tagId.getBytes().length,
+				tagId.getBytes(), (byte) memoryBank, (byte) address, count);
 	}
 
 	@Override
 	public void writeTagData(String password, int memoryBank, int address,
 			byte[] data) {
 
-		int types = -1;
-		
-		if (types == WRITE_EQUIPMENT_OPERATION_MEMORY
-				|| types == WRITE_EQUIPMENT_MEMORY
-				|| types == WRITE_USER_MEMORY) {
-			// byte[] epc = reader.stringToBytes(reader.m_strPCEPC);
-			byte[] epc = reader.stringToBytes(mPCEPC);
-			// TODO старый вариант
-			// byte memoryBank = USER_MEMORY_BANK; // user memory
-			// TODO старый вариант
-			// int address = 0; // пишем буфер с начала
-			// int dataLength = 32; // длина памяти данных
-			byte[] dataForWrite = new byte[50];
-			// int dataLength = Integer.valueOf(outBuffer.length) * 2;
-			// TODO старый вариант
-			// String passwordString = "00000000"; // пароль
-			// TODO старый вариант
-			// byte[] password = reader.stringToBytes(passwordString);
-
-			int rc = reader.Writelables(password.getBytes(), epc.length, epc,
-					(byte) memoryBank, (byte) address, (byte) 24, data);
-			System.arraycopy(data, 24, dataForWrite, 0, 24);
-
-			rc = reader.Writelables(password.getBytes(), epc.length, epc,
-					(byte) memoryBank, (byte) address + 48, (byte) 12,
-					dataForWrite);
-			/*
-			 * int rc = reader.Writelables(password, epc.length, epc,
-			 * memoryBank, (byte) address, (byte) realDataLength, dataForWrite);
-			 */
-			Message message = new Message();
-			if (rc >= 0) {
-				message.arg1 = RfidDriverBase.RESULT_RFID_SUCCESS;
-			} else {
-				message.arg1 = RfidDriverBase.RESULT_RFID_WRITE_ERROR;
-			}
-			mHandler.sendMessage(message);
-		}
+		// TODO реализовать получение Id метки
+		// TODO реализовать запись метки по полученому Id
 	}
 
 	@Override
 	public void writeTagData(String password, String tagId, int memoryBank,
 			int address, byte[] data) {
 
+		reader.m_handler = new writeTagDataHandler();
+
+		int rc = reader.Writelables(password.getBytes(),
+				tagId.getBytes().length, tagId.getBytes(), (byte) memoryBank,
+				(byte) address, data.length, data);
+
+		Message message = new Message();
+		if (rc >= 0) {
+			message.arg1 = RfidDriverBase.RESULT_RFID_SUCCESS;
+		} else {
+			message.arg1 = RfidDriverBase.RESULT_RFID_WRITE_ERROR;
+		}
+		mHandler.sendMessage(message);
 	}
 
 }
