4081f7260b2c0082d9e02c4169e7977b3ece6287
Dmitriy Logachov <demonwork@yandex.ru>
2015-12-15 16:30:24 +0500

    Добавлена категория для настроек драйверов считывателей. Так как после показа экрана настроек драйвера и возврата в основной экран настроек приложения, при изменении текущего драйвера не "выключался" экран настроек драйвера. То есть его нельзя было вызвать для драйверов у которых нет настроек, как и задумано, но визуально он отображался доступным для выбора. Код отвечающий за фрагмент настроек вынесен в отдельный класс.

--- Toir-mobile/src/ru/toir/mobile/ToirPreferences.java
+++ Toir-mobile/src/ru/toir/mobile/ToirPreferences.java
@@ -1,168 +1,19 @@
 package ru.toir.mobile;
 
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import dalvik.system.DexFile;
-import android.content.SharedPreferences;
+import ru.toir.mobile.fragments.ToirPreferenceFragment;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
-import android.util.Log;
-import android.view.View;
 
 public class ToirPreferences extends PreferenceActivity {
-
-	private static final String TAG = "ToirPreferences";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getFragmentManager().beginTransaction()
-				.replace(android.R.id.content, new ToirPreferenceFragment())
+		getFragmentManager()
+				.beginTransaction()
+				.replace(android.R.id.content,
+						new ToirPreferenceFragment(getApplicationContext()))
 				.commit();
-	}
-
-	public class ToirPreferenceFragment extends PreferenceFragment {
-
-		ListPreference drvList;
-		PreferenceScreen drvSettingScr;
-
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.preferences);
-
-			SharedPreferences preferences = PreferenceManager
-					.getDefaultSharedPreferences(getApplicationContext());
-
-			// получаем список драйверов по имени класса
-			List<String> driverClassList = new ArrayList<String>();
-			try {
-				DexFile df = new DexFile(getApplicationContext()
-						.getPackageCodePath());
-				for (Enumeration<String> iter = df.entries(); iter
-						.hasMoreElements();) {
-					String classPath = iter.nextElement();
-					if (classPath.contains("ru.toir.mobile.rfid.driver")
-							&& !classPath.contains("$")) {
-						driverClassList.add(classPath);
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			// строим список драйверов с именами и классами
-			Class<?> driverClass;
-			List<String> drvNames = new ArrayList<String>();
-			List<String> drvKeys = new ArrayList<String>();
-			for (String classPath : driverClassList) {
-
-				driverClass = null;
-				try {
-					// пытаемся получить класс драйвера
-					driverClass = Class.forName(classPath);
-					// пытаемся получить свойство DRIVER_NAME
-					String name = (String) (driverClass
-							.getDeclaredField("DRIVER_NAME").get(new String()));
-					if (name != null && !name.equals("")) {
-						drvNames.add(name);
-						drvKeys.add(classPath);
-					}
-				} catch (Exception e) {
-					Log.e(TAG, e.toString());
-				}
-			}
-
-			// элемент интерфейса со списком драйверов считывателей
-			drvList = (ListPreference) findPreference(getResources().getString(
-					R.string.rfidDriverListPrefKey));
-
-			// указываем названия и значения для элементов списка
-			drvList.setEntries(drvNames.toArray(new String[] {}));
-			drvList.setEntryValues(drvKeys.toArray(new String[] {}));
-
-			// при изменении драйвера, включаем дополнительный экран с
-			// настройками драйвера
-			drvList.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-
-				@Override
-				public boolean onPreferenceChange(Preference preference,
-						Object newValue) {
-
-					String value = (String) newValue;
-
-					// проверяем есть ли настройки у драйвера
-					if (getDrvSettingsSreen(value, drvSettingScr) != null) {
-						drvSettingScr.setEnabled(true);
-					} else {
-						drvSettingScr.setEnabled(false);
-					}
-
-					// принудительно перерисовываем элемент
-					View view = drvSettingScr.getView(null, null);
-					view.invalidate();
-					view = null;
-					return true;
-				}
-			});
-
-			drvSettingScr = (PreferenceScreen) findPreference(getResources()
-					.getString(R.string.rfidDrvSettingKey));
-
-			// проверяем есть ли настройки у драйвера
-			String currentDrv = preferences.getString(
-					getResources().getString(R.string.rfidDriverListPrefKey),
-					null);
-			if (currentDrv != null) {
-				if (getDrvSettingsSreen(currentDrv, drvSettingScr) != null) {
-					drvSettingScr.setEnabled(true);
-				} else {
-					drvSettingScr.setEnabled(false);
-				}
-			} else {
-				drvSettingScr.setEnabled(false);
-			}
-
-		}
-
-		private PreferenceScreen getDrvSettingsSreen(String classPath,
-				PreferenceScreen rootScreen) {
-
-			Class<?> driverClass;
-			Method method;
-			PreferenceScreen screen;
-
-			try {
-				// пытаемся получить класс драйвера
-				driverClass = Class.forName(classPath);
-
-				// пытаемся получить метод getSettingsScreen
-				method = driverClass.getMethod("getSettingsScreen",
-						new Class[] { PreferenceScreen.class });
-
-				// передаём драйверу "чистый" экран
-				rootScreen.removeAll();
-
-				// пытаемся вызвать метод
-				screen = (PreferenceScreen) method.invoke(null, rootScreen);
-
-				// возвращаем результат
-				return screen;
-			} catch (Exception e) {
-				Log.e(TAG, e.getLocalizedMessage());
-				return null;
-			}
-		}
-
 	}
 
 }
---
+++ Toir-mobile/src/ru/toir/mobile/fragments/ToirPreferenceFragment.java
@@ -0,0 +1,169 @@
+/**
+ * 
+ */
+package ru.toir.mobile.fragments;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import ru.toir.mobile.R;
+import dalvik.system.DexFile;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+
+/**
+ * @author koputo
+ * 
+ */
+public class ToirPreferenceFragment extends PreferenceFragment {
+
+	private static final String TAG = "ToirPreferenceFragment";
+
+	private ListPreference drvList;
+	private PreferenceScreen drvSettingScr;
+	private Context context;
+	private PreferenceCategory drvSettingCategory;
+
+	public ToirPreferenceFragment(Context context) {
+		this.context = context;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(context);
+
+		// получаем список драйверов по имени класса
+		List<String> driverClassList = new ArrayList<String>();
+		try {
+			DexFile df = new DexFile(context.getPackageCodePath());
+			for (Enumeration<String> iter = df.entries(); iter
+					.hasMoreElements();) {
+				String classPath = iter.nextElement();
+				if (classPath.contains("ru.toir.mobile.rfid.driver")
+						&& !classPath.contains("$")) {
+					driverClassList.add(classPath);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// строим список драйверов с именами и классами
+		Class<?> driverClass;
+		List<String> drvNames = new ArrayList<String>();
+		List<String> drvKeys = new ArrayList<String>();
+		for (String classPath : driverClassList) {
+
+			driverClass = null;
+			try {
+				// пытаемся получить класс драйвера
+				driverClass = Class.forName(classPath);
+				// пытаемся получить свойство DRIVER_NAME
+				String name = (String) (driverClass
+						.getDeclaredField("DRIVER_NAME").get(new String()));
+				if (name != null && !name.equals("")) {
+					drvNames.add(name);
+					drvKeys.add(classPath);
+				}
+			} catch (Exception e) {
+				Log.e(TAG, e.toString());
+			}
+		}
+
+		// категория с экраном настроек драйвера
+		drvSettingCategory = (PreferenceCategory) findPreference("drvSettingsCategory");
+
+		// элемент интерфейса со списком драйверов считывателей
+		drvList = (ListPreference) findPreference(getResources().getString(
+				R.string.rfidDriverListPrefKey));
+
+		// указываем названия и значения для элементов списка
+		drvList.setEntries(drvNames.toArray(new String[] {}));
+		drvList.setEntryValues(drvKeys.toArray(new String[] {}));
+
+		// при изменении драйвера, включаем дополнительный экран с
+		// настройками драйвера
+		drvList.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+			@Override
+			public boolean onPreferenceChange(Preference preference,
+					Object newValue) {
+
+				String value = (String) newValue;
+				PreferenceScreen screen = getDrvSettingsSreen(value,
+						drvSettingScr);
+
+				// проверяем есть ли настройки у драйвера
+				if (screen != null) {
+					drvSettingCategory.setEnabled(true);
+				} else {
+					drvSettingCategory.setEnabled(false);
+				}
+
+				return true;
+			}
+		});
+
+		drvSettingScr = (PreferenceScreen) findPreference(getResources()
+				.getString(R.string.rfidDrvSettingKey));
+
+		// проверяем есть ли настройки у драйвера
+		String currentDrv = preferences.getString(
+				getResources().getString(R.string.rfidDriverListPrefKey), null);
+		if (currentDrv != null) {
+			if (getDrvSettingsSreen(currentDrv, drvSettingScr) != null) {
+				drvSettingCategory.setEnabled(true);
+			} else {
+				drvSettingCategory.setEnabled(false);
+			}
+		} else {
+			drvSettingCategory.setEnabled(false);
+		}
+
+	}
+
+	private PreferenceScreen getDrvSettingsSreen(String classPath,
+			PreferenceScreen rootScreen) {
+
+		Class<?> driverClass;
+		Method method;
+		PreferenceScreen screen;
+
+		try {
+			// пытаемся получить класс драйвера
+			driverClass = Class.forName(classPath);
+
+			// пытаемся получить метод getSettingsScreen
+			method = driverClass.getMethod("getSettingsScreen",
+					new Class[] { PreferenceScreen.class });
+
+			// передаём драйверу "чистый" экран
+			rootScreen.removeAll();
+
+			// пытаемся вызвать метод
+			screen = (PreferenceScreen) method.invoke(null, rootScreen);
+
+			// возвращаем результат
+			return screen;
+		} catch (Exception e) {
+			Log.e(TAG, e.getLocalizedMessage());
+			return null;
+		}
+	}
+}
