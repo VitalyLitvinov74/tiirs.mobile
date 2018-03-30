de40728d05da65bc1ea55b140245f53e03afd56a
Olejek <olejek8@yandex.ru>
2018-03-30 14:37:24 +0500

    Добавлена работа с телеграмм Небольшие доработки

--- Toir-mobile/app/src/main/java/ru/toir/mobile/SettingsActivity.java
+++ Toir-mobile/app/src/main/java/ru/toir/mobile/SettingsActivity.java
@@ -432,7 +432,7 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
                 return false;
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.e(TAG, e.getLocalizedMessage());
             return false;
         }
 
